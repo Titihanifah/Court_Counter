d8be7eed7a5af43c0e454a21f4a528b093e2911e
TitiHanifah <titihanifah25@gmail.com>
2015-10-08 20:51:46 +0700

    belum jadi

--- app/src/main/java/id/tik/courtcounter/MainActivity.java
+++ app/src/main/java/id/tik/courtcounter/MainActivity.java
@@ -4,34 +4,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvScoreTeamA;
+    private TextView tvScoreTeamB;
+    private Button btn3A;
+    private Button btn2A;
+    private Button btnFreeA;
+    private Button btn3B;
+    private Button btn2B;
+    private Button btnFreeB;
+    private Button btnReset;
+    private int scoreTeamA;
+    private int scoreTeamB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void init()
+    {
+      tvScoreTeamA = (TextView) findViewById(R.id.tv_score_team_a);
+      tvScoreTeamB = (TextView) findViewById(R.id.tv_score_team_b);
+      btn3A = (Button) findViewById(R.id.btn_3A);
+      btn2A = (Button) findViewById(R.id.btn_2A);
+      btnFreeA = (Button) findViewById(R.id.btn_freeA);
+      btn3B = (Button) findViewById(R.id.btn_3B);
+      btn2B = (Button) findViewById(R.id.btn_2B);
+      btnFreeB = (Button) findViewById(R.id.btn_freeB);
+      scoreTeamA = scoreTeamB = 0;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
